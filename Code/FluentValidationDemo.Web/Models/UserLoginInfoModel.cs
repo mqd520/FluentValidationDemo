834e74c2648b2ebc2c1bbe9ca94d7c6b3b462f94
leon <1506816370@qq.com>
2020-12-13 17:58:49 +0800

    11

---
+++ Code/FluentValidationDemo.Web/Models/UserLoginInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FluentValidationDemo.Web.Models
+{
+    public class UserLoginInfoModel
+    {
+        public string UserName { get; set; }
+
+        public string Pwd { get; set; }
+    }
+}
---
+++ Code/FluentValidationDemo.Web/Validator/UserLoginInfoModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using FluentValidation;
+
+using FluentValidationDemo.Web.Models;
+
+namespace FluentValidationDemo.Web.Validator
+{
+    public class UserLoginInfoModelValidator : AbstractValidator<UserLoginInfoModel>
+    {
+        public UserLoginInfoModelValidator()
+        {
+            RuleFor(x => x.UserName)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("用户名不能为空");
+
+            RuleFor(x => x.Pwd)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("密码不能为空");
+        }
+    }
+}
